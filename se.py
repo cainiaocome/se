12af2730395d266d24ad883e825803ae27bd66a4
zhangjialin <cainiaocome@gmail.com>
2015-09-17 19:47:52 +0800

    apache wsgi

--- se.py
+++ se.py
@@ -4,18 +4,30 @@
 from flask import Flask
 from flask import url_for
 from flask import render_template
+from flask import request
+from flask import abort, redirect
+import string
 app = Flask(__name__)
 
+whitelist = string.letters + string.digits + '!"#$%&()*+,-./:;<=>?@[]^_`{|}~'
 
 @app.route('/')
 @app.route('/index')
 @app.route('/index.html')
 def index():
     return render_template('index.html')
-@app.route('/search/<keyword>')
-def search(keyword):
-    #print url_for('static', filename='style.css')
-    return 'search {}'.format(keyword)
+
+@app.route('/fuck')
+def fuck():
+    return 'fuck you, {}'.format(request.remote_addr)
+
+@app.route('/search', methods=['get'])
+def search():
+    kw = request.args.get('kw', '')
+    for x in kw:
+        if x not in whitelist:
+            return redirect(url_for('fuck'))
+    return 'search {}'.format(kw)
 
 if __name__=='__main__':
     # be careful, remember to disable debug before taking online
---
+++ se.wsgi
@@ -0,0 +1,7 @@
+import sys
+
+sys.path.insert(0, '/var/www/se/')
+from se import app as application
+
+
+# mod_wsgi file
